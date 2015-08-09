de99f3ffeb52bc4b6ede435cafd915460a87b3e3
niels <opensource21@gmail.com>
2015-08-09 15:52:33 +0200

    Add Javadoc

--- src/main/java/de/ppi/fuwesta/thymeleaf/springboot/Bootstrap2DialectConfig.java
+++ src/main/java/de/ppi/fuwesta/thymeleaf/springboot/Bootstrap2DialectConfig.java
@@ -5,9 +5,18 @@ import org.springframework.context.annotation.Configuration;
 
 import de.ppi.fuwesta.thymeleaf.bootstrap2.Bootstrap2Dialect;
 
+/**
+ * Autoconfiguration for Spring-boot.
+ * @author niels
+ *
+ */
 @Configuration
 public class Bootstrap2DialectConfig {
 
+    /**
+     * Create the {@link Bootstrap2Dialect}.
+     * @return the {@link Bootstrap2Dialect}.
+     */
     @Bean
     public Bootstrap2Dialect bootstrap2Dialect() {
         return new Bootstrap2Dialect();
